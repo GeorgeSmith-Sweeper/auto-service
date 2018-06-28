937d155af754a30c65e2bf4d735e2e07a4b300a4
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-06-28 17:16:49 -0400

    add first_name, last_name to user model and migration

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: {
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     username: {
       type: DataTypes.STRING,
+      unique: {
+        name: 'users_username',
+        msg: 'username not available',
+      },
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: {
+        name: 'users_email',
+        msg: 'A user with this email already exists',
+      },
       validate: {
         isEmail: true,
       },
